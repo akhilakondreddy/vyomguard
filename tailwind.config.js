3ff493a6fe11744b1629c86f851ead9a94d95476
Kondreddy Akhila <109570678+akhilakondreddy@users.noreply.github.com>
2025-11-17 14:11:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        vyom: "#ff7b00",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
